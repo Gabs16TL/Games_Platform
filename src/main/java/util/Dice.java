1e86b4cdb8c8c7aad88e0410a5fd3bf249ad3ad9
Jesus Barrera <jesus.barrera@telusinternational.com>
2022-07-25 11:16:25 -0600

    Se valido que el valor generado no fuera menor a 0

--- src/main/java/util/Dice.java
+++ src/main/java/util/Dice.java
@@ -4,6 +4,6 @@ import java.util.Random;
 
 public class Dice {
     public static int generateValue(int bound) {
-        return new Random().nextInt(bound);
+        return new Random().nextInt(bound + 1);
     }
 }
