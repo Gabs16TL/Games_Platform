000f71891279f6fa332826dcb39f65b34c59ada0
Jesus Barrera <jesus.barrera@telusinternational.com>
2022-07-25 11:02:42 -0600

    Se agrego test para ignorar el orden de ingreso de los caracteres

--- src/test/java/game/hangman/SecretWordTest.java
+++ src/test/java/game/hangman/SecretWordTest.java
@@ -7,16 +7,25 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class SecretWordTest {
+    private String word = "hello";
     private SecretWord secretWord;
 
     @Before
     public void init() {
-        secretWord = new SecretWord("hola");
+        secretWord = new SecretWord(word);
     }
 
     @Test
-    public void shouldAddUpperCaseLetter() {
-        assertTrue(secretWord.AddLetter('A'));
+    public void shouldIgnoreLetterCase() {
+        assertTrue(secretWord.AddLetter(word.toUpperCase().charAt(0)));
+    }
+
+    @Test
+    public void shouldIgnoreCharactersOrder() {
+        for (int i = (word.length() - 1); i >= 0; i--) {
+            secretWord.AddLetter(word.charAt(i));
+        }
+        assertTrue(secretWord.isGuessed());
     }
 
 }
